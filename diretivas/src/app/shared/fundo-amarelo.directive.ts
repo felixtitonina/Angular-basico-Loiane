127a56e45424340904e12029b32d90826e5df720
felixtitonina <felixtitonina@outlook.com>
2020-07-11 21:12:35 -0300

    Criando diretiva de ElementRef

--- diretivas/src/app/shared/fundo-amarelo.directive.ts
+++ diretivas/src/app/shared/fundo-amarelo.directive.ts
@@ -1,10 +1,25 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
+// import { Directive, ElementRef, Renderer } from '@angular/core';
 
 @Directive({
-  selector: '[fundoAmarelo]'
+  // selector: '[fundoAmarelo]'
+  // selector: 'p[fundoAmarelo]'
+  selector: 'button[fundoAmarelo]' // quando quero q seja aplicado apenas em um tag 
 })
 export class FundoAmareloDirective {
 
-  constructor() { }
+  constructor(private _elementRef: ElementRef) {
+    console.log(_elementRef)
+    // evitar usar ElementRef pois tem vulnerabilidade 
+    this._elementRef.nativeElement.style.backgroundColor = 'yellow'
+  }
+
+  // Renderer não funciona 
+  // constructor(private _elementRef: ElementRef, private _renderer: Renderer) {
+  //   console.log(_elementRef)
+  //   // evitar usar ElementRef pois tem vulnerabilidade 
+  //   // this._elementRef.nativeElement.style.backgroundColor = 'yellow'
+  //   this._renderer.setElement(this._elementRef.nativeElement, 'backgroundColor', 'yellho')
+  // }
 
 }

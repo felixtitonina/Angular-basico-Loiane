5c5649d7ffe7df71a32061a0c4f5326a5a2848da
felixtitonina <felixtitonina@outlook.com>
2020-07-12 16:32:49 -0300

    Criando service sem IDependencia

--- servicos/src/app/cursos/cursos.component.ts
+++ servicos/src/app/cursos/cursos.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CursosService } from './cursos.service';
+
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.css']
 })
 export class CursosComponent implements OnInit {
+  cursos: string[] = []
 
-  constructor() { }
+  cursosService : CursosService
+  
+  constructor() { // primeiro metodo a ser inicializado | cria a instancia
+  this.cursosService = new CursosService() //Criando uma instancia de uma class
 
-  ngOnInit(): void {
+  }
+
+  ngOnInit(): void { // primeiro metodo executado executado quando a class for instaciado
+    this.cursos = this.cursosService.getCursos()
   }
 
 }
---
+++ servicos/src/app/cursos/cursos.service.ts
@@ -0,0 +1,5 @@
+export class CursosService {
+    getCursos() {
+        return ["angular 2", "java", "nodejs"]
+    }
+}

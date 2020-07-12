d8894041617ccc2145142209ad55f3167ff5c89d
felixtitonina <felixtitonina@outlook.com>
2020-07-12 19:45:45 -0300

    Criando mais de uma instancia do service

--- servicos/src/app/app.module.ts
+++ servicos/src/app/app.module.ts
@@ -3,20 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CursosComponent } from './cursos/cursos.component';
+import { CursosModule } from './cursos/cursos.module';
 import { CursosService } from './cursos/cursos.service';
+import { CriarCursoModule } from './criar-curso/criar-curso.module';
 @NgModule({
   declarations: [
     AppComponent,
-    CursosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CriarCursoModule,
+    CursosModule
   ],
-  providers: [
-    CursosService,  // como estamos colocando o CursosService no app.module ele vai esta disponivel em toda a instancia da aplicação
-  ],
+  providers: [CursosService],  // como estamos colocando o CursosService no app.module ele vai esta disponivel em toda a instancia da aplicação
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- servicos/src/app/cursos/cursos.service.ts
+++ servicos/src/app/cursos/cursos.service.ts
@@ -3,7 +3,15 @@ import { Injectable } from "@angular/core"
 @Injectable()    // aqui falamos que esta classe é injetavel 
 // ou seja o angular vai poder intanciar esta classe quando for chamada 
 export class CursosService {
+    private cursos: string[] = ["angular 2", "java", "nodejs"] 
+    constructor(){
+        console.log("CursosService")
+    }
+
     getCursos() {
-        return ["angular 2", "java", "nodejs"]
+        return this.cursos
+    }
+    addCurso(curso: string){
+        this.cursos.push(curso)
     }
 }

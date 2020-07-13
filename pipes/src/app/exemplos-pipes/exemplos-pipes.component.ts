f79c9539c6f8da4ff848f2860345af4b9c3a29cf
felixtitonina <felixtitonina@outlook.com>
2020-07-13 00:24:00 -0300

    Entendendo pipes

--- pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
+++ pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exemplos-pipes.component.css']
 })
 export class ExemplosPipesComponent implements OnInit {
-
+  livro: any = {
+    titulo: 'Learning JavaScript Data Structures and Algorithms:',
+    rating: 4.54321,
+    nuemroPaginas: 314,
+    preco: 44.99,
+    dataLancamento: new Date(2016, 5, 23),
+    url: 'https://www.amazon.com.br/'
+  }
   constructor() { }
 
   ngOnInit(): void {

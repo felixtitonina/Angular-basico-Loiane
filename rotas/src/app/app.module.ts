3caead5bf867a4a6b32ad74f4fac3a3eb03fbd51
felixtitonina <felixtitonina@outlook.com>
2020-07-25 01:11:08 -0300

    Criado componente alunos com rotas

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -4,29 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-//import { CursosComponent } from './cursos/cursos.component';
 //import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
 //import { CursosService } from './cursos/cursos.service';
 //import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 import { CursosModule } from './cursos/cursos.module';
+import { AlunosModule } from './alunos/alunos.module';
 import { AppRoutingModule } from './app-routing.module';
 // import { routing } from './app-routing.module'
-import { AlunosComponent } from './alunos/alunos.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       HomeComponent,
       LoginComponent,
-      /*CursosComponent,
+      /*
       CursoDetalheComponent,
       CursoNaoEncontradoComponent,
-      */,
-      AlunosComponent
+      */
    ],
    imports: [
       BrowserModule,
       CursosModule,
+      AlunosModule,
       AppRoutingModule,
       //routing
    ],

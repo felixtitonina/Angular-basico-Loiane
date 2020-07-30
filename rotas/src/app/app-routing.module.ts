40a443222936fb818e44d6582e4f26f5fd781d77
felixtitonina <felixtitonina@outlook.com>
2020-07-29 23:32:43 -0300

    Entendendo rota canLoad caregar mod c permissao

--- rotas/src/app/app-routing.module.ts
+++ rotas/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
-import { AuthGuard } from './guards/auth.guard';
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './guards/auth.guard';
 // import { CursosComponent } from './cursos/cursos.component';
 // import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
 // import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
@@ -16,12 +16,14 @@ const AppRoutes: Routes = [
     path: 'cursos',
     loadChildren: () => import('./cursos/cursos.module').then(mod => mod.CursosModule),
     canActivate: [AuthGuard],
-    canActivateChild: [CursosGuard]
+    canActivateChild: [CursosGuard],
+    canLoad:[AuthGuard]
   },
   {
     path: 'alunos',
     loadChildren: () => import('./alunos/alunos.module').then(mod => mod.AlunosModule),
     canActivate: [AuthGuard],
+    canLoad:[AuthGuard]
     // canActivateChild: [AlunosGuard]
 
   },
--- rotas/src/app/guards/auth.guard.ts
+++ rotas/src/app/guards/auth.guard.ts
@@ -1,11 +1,12 @@
+import { Route } from '@angular/compiler/src/core';
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router'
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanLoad } from '@angular/router'
 import { Observable } from 'rxjs';
 
 import { AuthService } from './../login/auth.service';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
 
   constructor(
     private authService: AuthService,
@@ -18,9 +19,24 @@ export class AuthGuard implements CanActivate {
     state: RouterStateSnapshot
   ): Observable<boolean> | boolean {
     if (this.authService.usuarioEstaAutenticado()) {
-      return true      
+
+      console.log("canActivate");
+
+      return this.verificarAcesso()
     }
+  }
+  private verificarAcesso() {
+    if (this.authService.usuarioEstaAutenticado()) {
+      return true
+    }
+    console.log(("verificarAcesso"));
+
     this.router.navigate(['/login'])
     return false
+  }
+  canLoad(route: Route): Observable<boolean> | Promise<boolean> | boolean {
+   console.log("canlaod");
+
+    return this.verificarAcesso()
   }
 }
